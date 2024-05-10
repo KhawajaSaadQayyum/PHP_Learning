cf1feae4cbdd2e1d483f68f65c8eeeec7e53252d
Saad Qayyum <khawajasaadqayyum4110@gmail.com>
2024-05-10 14:14:04 +0500

    Today I'm gping to start lrearning PHP

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+echo "hello  saad world <br>" ;
+echo "Hello World <br>";
+
+echo "I am enjoying <br>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order a pizzas</button>
+</body>
+</html>
+
+
+
+
